6ce97aa3558615fdc1a52b3441854c9260ad75dc
lei <lizhilei126@163.com>
2023-03-10 05:00:58 -0800

    增加五个peer组织的网络

---
+++ fabirc-network/5orgs/setDNS.sh
@@ -0,0 +1,18 @@
+echo "127.0.0.1       council.ifantasy.net" >> /etc/hosts
+echo "127.0.0.1       soft.ifantasy.net" >> /etc/hosts
+echo "127.0.0.1       web.ifantasy.net" >> /etc/hosts
+echo "127.0.0.1       hard.ifantasy.net" >> /etc/hosts
+echo "127.0.0.1       org4.ifantasy.net" >> /etc/hosts
+echo "127.0.0.1       org5.ifantasy.net" >> /etc/hosts
+
+echo "127.0.0.1       orderer1.council.ifantasy.net" >> /etc/hosts
+echo "127.0.0.1       orderer2.council.ifantasy.net" >> /etc/hosts
+echo "127.0.0.1       orderer3.council.ifantasy.net" >> /etc/hosts
+echo "127.0.0.1       orderer4.council.ifantasy.net" >> /etc/hosts
+echo "127.0.0.1       orderer5.council.ifantasy.net" >> /etc/hosts
+
+echo "127.0.0.1       peer1.soft.ifantasy.net" >> /etc/hosts
+echo "127.0.0.1       peer1.web.ifantasy.net" >> /etc/hosts
+echo "127.0.0.1       peer1.hard.ifantasy.net" >> /etc/hosts
+echo "127.0.0.1       peer1.org4.ifantasy.net" >> /etc/hosts
+echo "127.0.0.1       peer1.org5.ifantasy.net" >> /etc/hosts
